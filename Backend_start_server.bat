071e94f00024a895fa3f1fe96ec3f8fcfdf139c4
adekola <gabkola94@gmail.com>
2025-03-12 13:37:07 +0100

    Automate server

---
+++ Backend_start_server.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\Black~~Knight\Desktop\Server Monitoring sys\New_Server-MONITOR\SMS_venv\Scripts"
+call activate
+cd ..
+cd ..
+cd backend
+python app.py
---
+++ start_frontend.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Black~~Knight\Desktop\Server Monitoring sys\New_Server-MONITOR\client\frontend"
+npm start
